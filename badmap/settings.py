7231daf04a62e04aeea2b3aaaa9fc7f7c17990af
DESKTOP-LJ3B2H2\mmx36 <mmx362003@gmail.com>
2017-06-07 01:31:20 +0800

    update setting

--- badmap/settings.py
+++ badmap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0h!pu3c+8p1p6$zd5l*c2y+(m%bxbeul22c3*8hph7nrxx&o3@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
