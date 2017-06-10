be9622f5762eeda028d046b389ed458f9d6289e5
JosephTsai <mmx362003@gmail.com>
2017-06-10 13:24:43 +0800

    trying to link db

--- cron.py
+++ cron.py
@@ -1,9 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
+import pymongo
+import os
 
+MONGO_URL = os.environ.get('MONGODB_URI')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/rest";
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
+    client = pymongo.MongoClient(MONGO_URL)
+    db = client.heroku_szv1xx0f
+    db.testing.insert_one({"x": 10})
     print('This job is run every one minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
