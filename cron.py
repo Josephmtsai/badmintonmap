6adf83b6c1a25c0d965f6929400174bd6fd1a445
XUENN\joseph.tsai <mmx362003@gmail.com>
2017-06-12 14:00:11 +0800

    adjsut to 6hour run a job

--- cron.py
+++ cron.py
@@ -11,7 +11,7 @@ from crawler import googleExcelCrawler
 MONGO_URL = os.environ.get('MONGODB_URI')
 if not MONGO_URL:
     MONGO_URL = "mongodb://localhost:27017/rest";
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=360)
 def timed_job():
     client = pymongo.MongoClient(MONGO_URL)
     db = client.heroku_szv1xx0f
@@ -19,10 +19,7 @@ def timed_job():
     if len(badmintonInfoList) > 0:
         db.badmintonInfo.insert_many(badmintonInfoList)
         print('This job is insert data')
-    print('This job is run every 3 minutes.')
+    print('This job is run every 360 minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
 
 sched.start()
