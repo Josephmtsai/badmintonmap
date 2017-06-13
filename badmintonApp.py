0e6a50fad2433f9e6f1753af5e7379f9ca8bf802
JosephTsai <mmx362003@gmail.com>
2017-06-13 21:30:09 +0800

    testing add the api and cron job

--- badmintonApp.py
+++ badmintonApp.py
@@ -1,9 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
+import pymongo
+from bson.json_util import dumps
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+dumps(l)
+api = Api(app)
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+class getBadmintonInfoList(Resource):
+    def get(self):
+        client = pymongo.MongoClient(MONGO_URL)
+        db = client.heroku_szv1xx0f
+        return dumps(db.badmintonInfo.find())
+api.add_resource(HelloWorld, '/')
+api.add_resource(getBadmintonInfoList,'/getBadmapInfoList')
 
 if __name__ == "__main__":
     app.run()
--- cron.py
+++ cron.py
@@ -5,14 +5,14 @@ sched = BlockingScheduler()
 import pymongo
 import os
 import datetime
-
+import requests
 from crawler import googleExcelCrawler
 
 MONGO_URL = os.environ.get('MONGODB_URI')
 if not MONGO_URL:
     MONGO_URL = "mongodb://localhost:27017/rest";
 @sched.scheduled_job('interval', minutes=360)
-def timed_job():
+def crawler_job():
     client = pymongo.MongoClient(MONGO_URL)
     db = client.heroku_szv1xx0f
     badmintonInfoList = googleExcelCrawler.syncExcelToDB(os.environ.get('GoogleAuthKey'),"1sdEYj_w57iQaFhD5eNNOMLEhMbzlnhs7vR8Lz5RlChA")
@@ -20,6 +20,12 @@ def timed_job():
         db.badmintonInfo.insert_many(badmintonInfoList)
         print('This job is insert data')
     print('This job is run every 360 minutes.')
+
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    response = requests.get(os.environ.get('CURRENTDOMAIN'))
+    print("Current Page Status " +str(response.status_code )
 
 
 sched.start()
