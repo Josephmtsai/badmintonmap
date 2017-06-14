3fb4b54ee6a04ee3ffc9321a2e6cc0c25a990181
JosephTsai <mmx362003@gmail.com>
2017-06-14 22:28:14 +0800

    chane cron job time

--- cron.py
+++ cron.py
@@ -7,11 +7,11 @@ from crawler import googleExcelCrawler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=10)
+@sched.scheduled_job('interval', minutes=360)
 def crawler_job():
     googleExcelCrawler.syncExcelToDB(os.environ.get('GoogleAuthKey'),"1sdEYj_w57iQaFhD5eNNOMLEhMbzlnhs7vR8Lz5RlChA")
     print('This job is insert data')
-    print('This job is run every 10 minutes.')
+    print('This job is run every 360 minutes.')
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     response = requests.get(os.environ.get('CURRENTDOMAIN'))
