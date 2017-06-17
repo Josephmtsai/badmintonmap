8f9b87d30e0079ac5721aa972bbbabb8f8454d47
AndyChen <shinningstar1001@gmail.com>
2017-06-18 02:38:18 +0800

    Add cors

--- badmintonApp.py
+++ badmintonApp.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, make_response
+from flask_cors import CORS
 import pymongo
 import os
 import settings 
@@ -9,12 +10,13 @@ import dbHandler
 
 app = Flask(__name__)
 api = Api(app)
+cors = CORS(app, resources={r"/api/*": {"origins": r"(.*).herokuapp.com"}})
 class HelloWorld(Resource):
     def get(self):
         return "你好"
 
 
-class getBadmintonInfoList(Resource):
+class BadmintonInfoList(Resource):
     def get(self):
         return make_response(dbHandler.dbHandler.getbadmintonInfoList())
 
@@ -23,7 +25,7 @@ class CrawlerBadmintonExcelList(Resource):
     def get(self):
         return googleExcelCrawler.syncExcelToDB(os.environ.get('GoogleAuthKey'),"1sdEYj_w57iQaFhD5eNNOMLEhMbzlnhs7vR8Lz5RlChA")
 api.add_resource(HelloWorld, '/')
-api.add_resource(getBadmintonInfoList,'/api/getBadmapInfoList')
+api.add_resource(BadmintonInfoList,'/api/badmintoninfolist')
 api.add_resource(CrawlerBadmintonExcelList,'/crawler')
 
 if __name__ == "__main__":
