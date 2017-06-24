4d0e1fb52360ac75c74b57d11c2ee1c62b021e03
josepht <mmx362003@gmail.com>
2017-06-24 19:22:18 +0800

    get phone and line info add regular expression

--- crawler/googleExcelCrawler.py
+++ crawler/googleExcelCrawler.py
@@ -4,14 +4,15 @@ from dbHandler import dbHandler
 from crawler import googleMapLocation
 from Common import Common
 import datetime
+import re
 def syncExcelToDB(apiKey,excelsheetid): 
 	sheetList = [u'日',u'一',u'二',u'三',u'四',u'五',u'六']
 	service = discovery.build('sheets', 'v4', developerKey=apiKey,discoveryServiceUrl='https://sheets.googleapis.com/$discovery/rest?version=v4')
-	result = service.spreadsheets().values().batchGet(spreadsheetId=excelsheetid,ranges=sheetList).execute()
+	result = service.spreadsheets().values().batchGet(spreadsheetId=excelsheetid,ranges=sheetList,valueRenderOption='FORMULA').execute()
 	responseSheet = result.get('valueRanges', [])	
 	weekDay = Common.getTodayWeekDay()
-	existlocationDict =googleMapLocation.getExistLocationToDict()
 	newLocationDict = {}
+	existlocationDict =googleMapLocation.getExistLocationToDict()
 	badmintonInfoList = []
 	coordinate = None
 	address = None
@@ -23,8 +24,11 @@ def syncExcelToDB(apiKey,excelsheetid):
 			badmintonInfo = {}
 			try:
 				if len(row) > 8:
-					badmintonInfo['location'] = row[1]
-					print(existlocationDict)
+					if "HYPERLINK" in row[1]:
+						badmintonInfo['location'] = re.search(r'=HYPERLINK\("(.*)","(.*)"\)',row[1]).group(2)
+					else:
+						badmintonInfo['location'] = row[1]
+					#print(existlocationDict)
 					if existlocationDict is not None and badmintonInfo['location'].encode('UTF-8') not in existlocationDict:
 						coordinate,address,status =  googleMapLocation.getLocationInfo(row[3],apiKey) 
 					if coordinate is not None:
@@ -35,14 +39,20 @@ def syncExcelToDB(apiKey,excelsheetid):
 							newLocationDict = googleMapLocation.locationToDict(newLocationDict,existlocationDict,badmintonInfo['location'],address,coordinate)
 					badmintonInfo['payInfo'] = Common.convertToInt(row[6])
 					badmintonInfo['contactName'] = row[7]
-					badmintonInfo['contactPhone'] = row[8]
+					if  row[8] != "" and "HYPERLINK" in row[8]:
+						badmintonInfo['contactPhone'] = re.search(r'=HYPERLINK\("(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/(.*)","(.*)"\)',row[8]).group(5)
+					else:
+						badmintonInfo['contactPhone'] = row[8]
 					badmintonInfo['startTime'] = Common.convertToDateTime(row[0].split("~")[0],index -weekDay)	
 					badmintonInfo['endTime'] = Common.convertToDateTime(row[0].split("~")[1],index -weekDay)
 					badmintonInfo['weekDay'] = sheetList[index]
 					badmintonInfo['weekDayInt'] = index
 					badmintonInfo['source'] = "excel"
-					badmintonInfo['line'] = row[9] if len(row) > 9 else ""
-					badmintonInfo['sourceData'] = ",".join(row ) 
+					if len(row) > 9 and "HYPERLINK" in row[1]:
+						badmintonInfo['line'] = re.search(r'=HYPERLINK\("(.*)","(.*)"\)',row[1]).group(1)
+					else :
+						badmintonInfo['line'] = ""
+					badmintonInfo['sourceData'] = row 
 					#break
 			finally:					
 				badmintonInfoList.append(badmintonInfo)
