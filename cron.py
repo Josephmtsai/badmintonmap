81e3664d01da335f773738099e0e73950960c339
JosephTsai <mmx362003@gmail.com>
2017-09-26 23:03:18 +0800

    sync job change interval time

--- cron.py
+++ cron.py
@@ -7,12 +7,12 @@ from crawler import googleExcelCrawler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=360)
+@sched.scheduled_job('interval', minutes=720)
 def crawler_job():
     googleExcelCrawler.syncExcelToDB(os.environ.get('GoogleAuthKey'),"1d2a4Zdl4PuXCsUPbqW-r3vGtwxJKoZs0oxQy4YsK4Wg")
     print('This job is insert data')
-    print('This job is run every 360 minutes.')
-@sched.scheduled_job('interval', minutes=3)
+    print('This job is run every 720 minutes.')
+@sched.scheduled_job('interval', minutes=60)
 def timed_job():
     response = requests.get(os.environ.get('CURRENTDOMAIN'))
     print("Current Page Status " +str(response.status_code ))
