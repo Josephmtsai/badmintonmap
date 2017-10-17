960055487a0f0382388d58c79a8f045f52580089
Josephmtsai <Josephmtsai@users.noreply.github.com>
2017-10-17 18:32:01 +0800

    change file id

--- cron.py
+++ cron.py
@@ -9,7 +9,7 @@ sched = BlockingScheduler()
 #test
 @sched.scheduled_job('interval', minutes=720)
 def crawler_job():
-    googleExcelCrawler.syncExcelToDB(os.environ.get('GoogleAuthKey'),"1d2a4Zdl4PuXCsUPbqW-r3vGtwxJKoZs0oxQy4YsK4Wg")
+    googleExcelCrawler.syncExcelToDB(os.environ.get('GoogleAuthKey'),"1--VQHKN0mG-1CwxS_QKbpvFxtfQsDYrUS6PO-fDeIJo")
     print('This job is insert data')
     print('This job is run every 720 minutes.')
 @sched.scheduled_job('interval', minutes=60)
