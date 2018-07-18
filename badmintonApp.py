c55ffb5dc8c41fb2106be61b28164162ffe3c11e
Joseph <mmx362003@gmail.com>
2018-07-18 12:31:58 +0800

    Add get method
    A

--- badmintonApp.py
+++ badmintonApp.py
@@ -53,6 +53,8 @@ class LineBotHandler(Resource):
         except InvalidSignatureError:
             abort(400)
         return 'OK'
+    def get(self):
+        return 'OK'
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
