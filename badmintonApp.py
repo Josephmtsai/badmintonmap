26e6aa45a535a57e8b661deb743a5ef786e48c4a
Joseph <mmx362003@gmail.com>
2018-07-18 11:50:23 +0800

    fix bug

--- badmintonApp.py
+++ badmintonApp.py
@@ -44,15 +44,15 @@ class CrawlerBadmintonExcelList(Resource):
 class LineBotHandler(Resource):
     def post(self):
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+        signature = request.headers['X-Line-Signature']
+        # get request body as text
+        body = request.get_data(as_text=True)
+        # handle webhook body
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+        return 'OK'
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
